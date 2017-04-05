7a02db3e9b8405e6ff1eb5d81afc7dc3e4b4b895
itzel <itzeljimenez.do@gmail.com>
2017-04-04 23:32:11 -0500

    buscaminas estático Terminado

--- assets/main.js
+++ assets/main.js
@@ -1,42 +1,85 @@
 //
-
+var recargar =document.getElementById('reiniciar');
 var vaciosLength=document.getElementsByClassName('vacio').length;
 var bombas=document.getElementsByClassName('bomb');
-var num=document.getElementsByClassName('num').length;
 var tabla = document.getElementById('tabla');
+var num1=document.getElementsByClassName('num1');
+var num2=document.getElementsByClassName('num2');
+var num3=document.getElementsByClassName('num3');
 var arreglo = [];
-//eventos
+var numi1=[];
+var numi2=[];
+//Class num1
+for(var i =0; i<num1.length;i++){
+	numi1[i] = num1[i];
+}
+for ( var i = 0; i<num1.length; i++){
+	numi1[i].addEventListener("click", uno);
+}
+function uno (){
+	this.setAttribute("value", 1);
+	this.style.backgroundColor = "#A6ACAF"
+	this.className = "apretado";
+}
+//Class Num2
+for(var i =0; i<num2.length;i++){
+	numi2[i] = num2[i];
+}
+for ( var i = 0; i<num2.length; i++){
+	numi2[i].addEventListener("click", dos);
+}
+function dos (){
+	this.setAttribute("value", 2);
+	this.style.backgroundColor = "#A6ACAF"
+	this.className = "apretado";
+}
+//Class Num3
+num3[0].addEventListener("click", tres);
+function tres(){
+	this.setAttribute("value", 3);
+	this.style.backgroundColor = "#A6ACAF"
+	this.className = "apretado";
+}
 
-
+//Class Vacios
 for(var i =0; i<vaciosLength;i++){
 	arreglo[i] = document.getElementsByClassName('vacio')[i];
 }
 for ( var i = 0; i<vaciosLength; i++){
 	arreglo[i].addEventListener("click", color)
 }
-function color(){	
+function color(){
 		this.className = "apretado";
-		this.style.backgroundColor = "white";
+		this.style.backgroundColor = "#A6ACAF";
 }
+//Class Bombas
 	for ( var i = 0; i<bombas.length; i++){
 		bombas[i].addEventListener("click", bomba);
 	}
-	for ( var i = 0; i<num.length; i++){
-		num[i].addEventListener("click", color)
-	}
+
 function bomba(){
 	for (var i=0; i<bombas.length;i++){
-		bombas[i].style.background = "red";
+		bombas[i].style.background = "";
+		bombas[i].value = "💣";
 	}
-	alert("GAME OVER")
+	perdiste();
 }
 
-/*
-vacios[0].addEventListener("click", color);
-vacios[1].addEventListener("click", color);
-vacios[2].addEventListener("click", color);
-vacios[3].addEventListener("click", color);
-vacios[4].addEventListener("click", color);
-vacios[5].addEventListener("click", color);
+function perdiste(){
+	var status = document.getElementById('status');
+	status.innerText = "GAME OVER";
+	deshabilitar();
+}
 
-*/
+function deshabilitar(){
+	var botones = document.getElementsByTagName('input');
+	for (var i=0;botones.length;i++){
+		botones[i].disabled = "true";
+	}
+}
+
+function reiniciar(){
+	location.reload();
+}
+
+recargar.addEventListener("click", reiniciar)
